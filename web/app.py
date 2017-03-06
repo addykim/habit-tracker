ce222f15d6bcc707ec26fd42f4c8f6cdde028177
Addy Kim <soyaddyk@gmail.com>
2017-03-06 16:02:41 -0600

    Copy signed cookies login section

--- web/app.py
+++ web/app.py
@@ -12,7 +12,8 @@ def do_login():
     password = request.forms.get('password')
     # TODO Return something else than just string
     if check_login(username, password):
-        return "<p>Your login information was correct.</p>"
+        response.set_cookie("account", username, secret='some-secret-key')
+        return template("<p>Welcome {{name}}! You are now logged in.</p>", name=username)
     else:
         return "<p>Login failed.</p>"
 
@@ -55,7 +56,11 @@ def server_static(filename):
 
 @route('/restricted')
 def restricted():
-    abort(401, "Sorry, access denied.")
+    username = request.get_cookie("account", secret='some-secret-key')
+    if username:
+        return template("Hello {{name}}. Welcome back.", name=username)
+    else:
+        return "You are not logged in. Access denied."
 
 @error(404)
 def error404(error):
