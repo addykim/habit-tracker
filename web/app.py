e4b74214aac12c625fda51ed4aa658e6c3499a2b
Addy Kim <soyaddyk@gmail.com>
2017-03-06 18:19:40 -0600

    Created initial skeleton of api

--- web/app.py
+++ web/app.py
@@ -6,17 +6,7 @@ from bottle import abort, error, get, post, request, route, run, static_file, te
 def index():
     return dict()
 
-@post('/login') # or @route('/login', method='POST')
-def do_login():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    # TODO Return something else than just string
-    if check_login(username, password):
-        response.set_cookie("account", username, secret='some-secret-key')
-        return template("<p>Welcome {{name}}! You are now logged in.</p>", name=username)
-    else:
-        return "<p>Login failed.</p>"
-
+########## Utility Functions ##########
 # TODO remove hardcoded login answer
 def check_login(username=None, password=None):
     if username == 'Meow':
@@ -27,7 +17,7 @@ def check_login(username=None, password=None):
 ########## API ##########
 @get('/habit/<habitid>')
 def get_habit(habitid):
-    return {"habitid": habitid, "name": "Programming": "userid": 3, "streak": 3}
+    return {"habitid": habitid, "name": "Programming", "userid": 3, "streak": 3}
 
 @get('/user/<userid>')
 def get_user(userid):
@@ -35,18 +25,34 @@ def get_user(userid):
 
 @post('/habit')
 def create_habit():
-    name = requests.forms.get('name')
+    name = request.forms.get('name')
+    goal_streak = request.forms.get('goal_streak')
+    # swap out today's date for habit_started date"
+    # add github issue to handle different date zones
+    habit_started = "today"
+    print('created habit')
 
-@post('/user')
-def create_user():
+@post('/user/register')
+def register_user():
     name = requests.form.get('name')
     email = requests.form.get('email')
 
-# @put('/habit')
-# def update_habit():
+@post('/login')
+def login_user():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    # TODO Return something else than just string
+    if check_login(username, password):
+        response.set_cookie("account", username, secret='some-secret-key')
+        return template("<p>Welcome {{name}}! You are now logged in.</p>", name=username)
+    else:
+        return "<p>Login failed.</p>"
 
-# @put('/user/<userid>')
-# def update_user():
+@put('/habit')
+def update_habit():
+
+@put('/user/<userid>')
+def update_user():
 
 ########## Interally Used Routes ##########
 
