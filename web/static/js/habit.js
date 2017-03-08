951b75a851dba4c138d4420848661f634941d2b5
Addy Kim <soyaddyk@gmail.com>
2017-03-08 11:28:08 -0600

    Bootstrapize habits form

--- web/static/js/habit.js
+++ web/static/js/habit.js
@@ -5,16 +5,23 @@ class Habit extends Component {
   render() {
     return (
       <div>
-        <b>Create a habit</b>
-        <form action="/habit" method="post">
-          <p>I want to do <input name="name" type="text" /> for <input name="goal_streak" type="number"/> of days.</p>
-          <input value="Create" type="submit" />
-        </form>
-
-        <form action="/habit/3" method="post"> 
-          <input value="I did it" type="submit" />
-        </form>
-        <Button bsSize="large" bsStyle="primary">I did it!</Button>
+      <Form inline>
+        <FormGroup controlId="formInlineName">
+          <ControlLabel>I want to do </ControlLabel>
+          {' '}
+          <FormControl type="text" placeholder="habit" />
+        </FormGroup>
+        {' '}
+        <FormGroup controlId="formInlineEmail">
+          <ControlLabel>for </ControlLabel>
+          {' '}
+          <FormControl type="number" placeholder="21" />
+        </FormGroup>
+        {' '}
+        days
+        <Button type="submit">Submit</Button>
+      </Form>
+      <StreakView/>
       </div>
     );
   }
