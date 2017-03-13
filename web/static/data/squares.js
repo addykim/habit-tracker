b81ce36cc2a4858b430bc38cd9b2bfc13fca3720
Addy Kim <soyaddyk@gmail.com>
2017-03-13 13:54:10 -0500

    Add dummy data for a streak

---
+++ web/static/data/squares.js
@@ -0,0 +1,33 @@
+export default {
+  "name": "Programming",
+  "streak": [
+    {
+      "date": "2017-03-01",
+      "completed": false
+    },
+    {
+      "date": "2017-03-02",
+      "completed": false
+    },
+    {
+      "date": "2017-03-03",
+      "completed": false
+    },
+    {
+      "date": "2017-03-04",
+      "completed": false
+    },
+    {
+      "date": "2017-03-05",
+      "completed": false
+    },
+    {
+      "date": "2017-03-06",
+      "completed": true
+    },
+    {
+      "date": "2017-03-07",
+      "completed": false
+    }
+  ]
+}
