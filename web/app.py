c29bc6b431969ac5b2ba0d6bd5dd907214312de4
Addy Kim <soyaddyk@gmail.com>
2017-03-14 14:03:31 -0500

    Read in dummy users as json

--- web/app.py
+++ web/app.py
@@ -1,24 +1,5 @@
+import json
 from bottle import abort, error, get, post, put, request, route, run, static_file, template, view
-
-TEMP_HABIT = {
-    "habitid": 1, 
-    "name": "Programming", 
-    "startdate": "2017-05-03",
-    "userid": 3
-    }
-
-TEMP_USER = {
-    "userid": 1, 
-    "name": 
-    "Jane Kim", 
-    "email": "jane@gmail.com"
-    } 
-
-########## User Facing Routes ##########
-@route('/')
-@view('index.html')
-def index():
-    return {'title':'Index'}
 
 ########## Utility Functions ##########
 # TODO remove hardcoded login answer
@@ -28,22 +9,55 @@ def check_login(username=None, password=None):
             return True
     return False
 
+def read_json_as_string(path):
+    output = ''
+    for line in open(path):
+        output = output + line
+    return output
+
+########## DEBUG CONSTANTS ##########
+TEMP_HABIT = {
+    "habitid": 1, 
+    "name": "Programming", 
+    "startdate": "2017-05-03",
+    "userid": 3
+    }
+
+TEMP_USER = read_json_as_string('./static/data/users.json')
+
+########## User Facing Routes ##########
+@route('/')
+@view('index.html')
+def index():
+    return {'title':'Index'}
+
 ########## API ##########
 @get('/habit/<habitid>')
 def get_habit(habitid):
     return TEMP_HABIT
 
+@get('/user')
 @get('/user/<userid>')
-def get_user(userid):
-    return TEMP_USER
+def get_user(userid=None):
+    if userid is None:
+        print(json.loads(TEMP_USER))
+        return TEMP_USER
+    print(json.loads(TEMP_USER))
+    users = json.loads(TEMP_USER)
+    print(userid)
+    for user in users:
+        # FIXME
+        if user.get('id') == userid:
+            print('Found match:', user)
+            return json.dumps(user)
+    return 'User Not Found'
 
 @post('/habit')
 def create_habit():
-    name = request.forms.get('name')
-    goal_streak = request.forms.get('goal_streak')
+    # name = request.forms.get('habitName')
+    # goal_streak = request.forms.get('goalStreak')
     # swap out today's date for habit_started date"
     # add github issue to handle different date zones
-    habit_started = "today"
     print('created habit')
 
 @post('/user/register')
@@ -72,9 +86,9 @@ def update_user():
 
 ########## Interally Used Routes ##########
 
-@route('/static/<filename>')
-def server_static(filename):
-    return static_file(filename, root='./static')
+# @route('/static/<filename:path>')
+# def server_static(filename):
+    # return static_file(filename, root='./static/')
 
 @route('/dist/<filename>')
 def dist_static(filename):
