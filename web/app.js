53f3375540b9c6934ea9b4a6541b8e993cb88e53
Addy Kim <soyaddyk@gmail.com>
2017-03-15 18:59:31 -0500

    Copy example of login logic for express to app

--- web/app.js
+++ web/app.js
@@ -8,17 +8,68 @@ app.use('/api', require('./routes/habit'))
 
 app.use('/public', express.static(__dirname + '/public'))
 
+// dummy database
+var users = {
+  tj: { name: 'tj' }
+};
+
 app.get('/', function(req, res) {
   res.send('Hello world!')
 })
 
-function checkAuthenticated(res) {
-  res.status(401).send('You are not logged in. Access denied')
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function(req, res){
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
+    }
+  });
+});
+
+app.get('/logout', function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
 }
 
-function notFound(res) {
-  res.status(404).send('404 Not Found');
-}
+app.get('/restricted', restrict, function(req, res){
+  res.status(401).send('You are not logged in. Access denied')
+})
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
+  res.status(404).send('404 Not Found');  
+})
 
 app.listen(8080, function() {
   console.log('Listening on port 8080')
