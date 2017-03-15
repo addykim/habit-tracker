5307977b7ec10aabbc928bc735d2035974c4c47d
Addy Kim <soyaddyk@gmail.com>
2017-03-15 16:50:46 -0500

    Combine app and index into one file

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Grid, Row} from 'react-bootstrap';
+import Habit from './habit';
+import Login from './login';
 
-import App from './app';
+
+const gridInstance = (
+  <Grid>
+    <Row>
+      <ul
+          className="nav-bar">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </Row>
+    <Row>
+      <Habit/>
+    </Row>
+  </Grid>
+);
+
+class App extends Component {
+  render() {
+    return gridInstance;
+  }
+}
 
 ReactDOM.render(<App/>, document.getElementById('content'));
