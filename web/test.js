3449fd17207060f705ae6d1c78d379793b9218ee
Addy Kim <addysgkim@gmail.com>
2017-05-15 23:45:50 -0700

    Initial mocha testing - no actual tests run

---
+++ web/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+
+describe('habits', () => {
+  describe('create new habit', () => {
+    // assert
+    it('invalid submission', () => {
+
+    })
+    it('correct submission', () => {
+
+    })
+  })
+  // describe('markCompleted', () => {
+  //   it('mark completed', () => {
+  //     // assert
+  //   })
+  // })
+})
